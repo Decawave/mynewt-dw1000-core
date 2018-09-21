2437f2dd89bf9c8d4ff1b5dcf78d139baeb665fb
Paul Kettle <paul.kettle@gmail.com>
2018-09-21 08:31:26 -0700

    Modified DS_TWR_EXT state machine to include dw1000_phy_frame_duration calculation.

--- hw/drivers/dw1000/src/dw1000_rng.c
+++ hw/drivers/dw1000/src/dw1000_rng.c
@@ -797,7 +797,7 @@ rng_rx_complete_cb(dw1000_dev_instance_t * inst)
                                 + config->rx_timeout_period        
                                 + config->tx_holdoff_delay;         // Remote side turn arroud time. 
                             dw1000_set_rx_timeout(inst, timeout); 
-                                         
+
                             if (dw1000_start_tx(inst).start_tx_error){
                                 if(!(SLIST_EMPTY(&inst->extension_cbs))){
                                     dw1000_extension_callbacks_t *temp = NULL;
@@ -928,7 +928,10 @@ rng_rx_complete_cb(dw1000_dev_instance_t * inst)
                             dw1000_write_tx_fctrl(inst, sizeof(ieee_rng_response_frame_t), 0, true); 
                             dw1000_set_wait4resp(inst, true);    
                             dw1000_set_delay_start(inst, response_tx_delay);   
-                            dw1000_set_rx_timeout(inst, config->rx_timeout_period); 
+                            uint16_t timeout = dw1000_phy_frame_duration(&inst->attrib, sizeof(ieee_rng_response_frame_t)) 
+                                + config->rx_timeout_period        
+                                + config->tx_holdoff_delay;         // Remote side turn arroud time. 
+                            dw1000_set_rx_timeout(inst, timeout); 
 
                             if (dw1000_start_tx(inst).start_tx_error){
                                 if(!(SLIST_EMPTY(&inst->extension_cbs))){
@@ -987,7 +990,11 @@ rng_rx_complete_cb(dw1000_dev_instance_t * inst)
                             dw1000_write_tx_fctrl(inst, sizeof(twr_frame_t), 0, true); 
                             dw1000_set_wait4resp(inst, true);    
                             dw1000_set_delay_start(inst, response_tx_delay);   
-                            dw1000_set_rx_timeout(inst, config->rx_timeout_period); 
+                            uint16_t timeout = dw1000_phy_frame_duration(&inst->attrib, sizeof(ieee_rng_response_frame_t)) 
+                                + config->rx_timeout_period        
+                                + config->tx_holdoff_delay;         // Remote side turn arroud time. 
+                            dw1000_set_rx_timeout(inst, timeout); 
+
                         
                             if (dw1000_start_tx(inst).start_tx_error){
                                 if(!(SLIST_EMPTY(&inst->extension_cbs))){
