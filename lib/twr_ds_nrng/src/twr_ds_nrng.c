0093a3eb1039c8475fd0d9b61047c7b3cc18cd21
Anantha Raman KV <anantha.r@pathpartnertech.com>
2018-11-19 12:02:07 +0530

    Corrected the wrong sem count check

--- lib/twr_ds_nrng/src/twr_ds_nrng.c
+++ lib/twr_ds_nrng/src/twr_ds_nrng.c
@@ -212,7 +212,7 @@ rx_complete_cb(dw1000_dev_instance_t * inst, dw1000_mac_interface_t * cbs)
     if(inst->fctrl != FCNTL_IEEE_N_RANGES_16){
         return false;
     }
-    if(os_sem_get_count(&inst->nrng->sem) == 0)
+    if(os_sem_get_count(&inst->nrng->sem) == 1)
         return false;
     assert(inst->nrng);
     dw1000_nrng_instance_t * nrng = inst->nrng;
