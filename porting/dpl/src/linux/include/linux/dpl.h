0780259107d60d90bed82e5e855efc517b948399
Paul Kettle <paul.kettle@gmail.com>
2019-06-25 13:39:33 -0700

    Initial commit of Decawave Porting Layer (DPL)

---
+++ porting/dpl/src/linux/include/linux/dpl.h
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef _DPL_H_
+#define _DPL_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "linux/dpl_atomic.h"
+#include "linux/dpl_callout.h"
+#include "linux/dpl_error.h"
+#include "linux/dpl_eventq.h"
+#include "linux/dpl_os_mutex.h"
+#include "linux/dpl_os_types.h"
+#include "linux/dpl_os.h"
+#include "linux/dpl_sem.h"
+#include "linux/dpl_tasks.h"
+#include "linux/dpl_time.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _DPL_H_ */
